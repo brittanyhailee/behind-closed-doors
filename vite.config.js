3817cf51c893c5acea2095c4f7b9f327af068e57
Brittany Chan <brittanyhaileec@gmail.com>
2025-07-21 00:59:03 -0700

    added chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: { chunkSizeWarningLimit: 1600, }, // Added
   base: '/behind-closed-doors/'
 })
