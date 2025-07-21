39e67bdf2ad369e2f99633c99a0f632edc40baf2
Brittany Chan <brittanyhaileec@gmail.com>
2025-07-21 07:40:08 -0700

    changed design of textOverlay in ImgTransition

--- src/lib/ImgTransition.svelte
+++ src/lib/ImgTransition.svelte
@@ -115,9 +115,32 @@
         align-self: center;
         margin: 0;
         margin-top: 200px;
-        background-image: url('/public/ripped_paper2.png');
         background: rgba(0,0,0,0.5);
         text-align: center;
+
+        
+        /* margin: 50vh auto; */
+        width: 50%;
+        border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
+        border-style: solid;
+        border-width: 2px;
+        box-shadow: rgba(160, 82, 45, .4) 15px 28px 25px -18px;
+        box-sizing: border-box;
+        color: white;
+        cursor: pointer;
+        display: inline-block;
+        font-size: 1.2rem;
+        outline: none;
+        padding: .75rem;
+        text-decoration: none;
+        transition: all 235ms ease-in-out;
+        border-bottom-left-radius: 15px 255px;
+        border-bottom-right-radius: 225px 15px;
+        border-top-left-radius: 255px 15px;
+        border-top-right-radius: 15px 225px;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
 
 
     }
